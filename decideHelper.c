0ad0a645e3337154045decd7441f4a14f8379bf5
Borwornwat <borwornwat.klw@student.mahidol.ac.th>
2025-12-24 23:45:45 +0700

    Create decideHelper.c

---
+++ decideHelper.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc,char* argv[]){
+    srand(time(NULL));
+    if(argc<2){
+        printf("Type something");
+    }
+    if(strcmp(argv[1],"coin") == 0){
+        int n = (rand() % 2);
+        if(n == 1){
+            printf("Tail!!\n");
+        }
+        else
+        printf("Head!!\n");
+        return 0;
+    }
+    if(strcmp(argv[1],"wheel") == 0){
+        int max;
+        printf("How many choice? : ");
+        scanf("%d",&max);
+        char wheel[max][100];
+        for(int i=0; i<max; i++){
+            printf("choice: ");
+            scanf("%s",wheel[i]);
+        }
+        int m = (rand() % max);
+        printf("Your result is: %s\n",wheel[m]);
+        return 0;
+    }
+    if((strcmp(argv[1],"coin") != 0) && (strcmp(argv[1],"wheel") != 0)){
+        printf("We can't do that");
+    }
+    return 0;
+}
